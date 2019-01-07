57eb030dc8c91e0d8f31b9020bca099550fc27b0
Андрей Лямин <ghappyjohnny@gmail.com>
2019-01-08 00:20:16 +0300

    final not formatted version. But working!

--- mario.js
+++ mario.js
@@ -1,14 +1,40 @@
-let steps = Math.round(document.getElementsByName('pyramidH')[0].value / 100 * 19) + 1;
-let symbol = document.getElementsByName('symbol')[0].value;
-console.log(symbol);
+let steps = document.getElementsByName('pyramidH')[0];
+let stepsValue = Math.round(document.getElementsByName('pyramidH')[0].value / 100 * 19) + 1;
+let symbol = document.getElementById('symbol');
 
-drawPyramid(steps, symbol);
+steps.insertAdjacentHTML("afterend", "This is my caption.");
 
-function drawPyramid(height) {
+steps.addEventListener('change', function() {
+    drawPyramid(Math.round(document.getElementsByName('pyramidH')[0].value / 100 * 19) + 1, symbol.value);
+})
+symbol.addEventListener('change', function() {
+    drawPyramid(Math.round(document.getElementsByName('pyramidH')[0].value / 100 * 19) + 1, symbol.value);
+});
+
+drawPyramid(stepsValue, symbol.value);
+
+function drawPyramid(height, symb) {
+    console.log(height);
+    deletePyramid();
     let pyramidNode = document.getElementById("pyramid");
 
     for (let i = 1; i < height + 1; i++) {
     	let step = document.createElement("p");
+        step.textContent = 
+        '\u00A0'.repeat(height - i) + symb.repeat(i + 1);
+    	pyramidNode.appendChild(step);
+    }
+    document.getElementsByName('pyramidH')[0].nextSibling.textContent = height;
+}
+
+function deletePyramid() {
+    let pyramidNodeArray = document.getElementById("pyramid").childNodes;
+    for (let i = pyramidNodeArray.length - 1; i != -1 ; i--) {
+        pyramidNodeArray[i].remove();
+        console.log('cleared');
+    }
+}
+
      //    for (let j = 0; j < height - i; j++) {
      //        let divSpace = document.createElement('div');
      //        divSpace.className = 'space';
@@ -19,10 +45,3 @@ function drawPyramid(height) {
      //        divHash.className = 'hash';
      //        step.appendChild(divHash);
      //    }
-        step.textContent = '\u00A0'.repeat(height - i) + symbol.repeat(i + 1);
-    	pyramidNode.appendChild(step);
-    }
-    document.getElementById('construction').remove();
-}
-
-
